6cd87daac6b97239f628864a701f17cadb11531f
Moabe <moabeluis2018@gmail.com>
2022-02-21 22:22:09 -0300

    adicionando a box-Shadow

--- App.tsx
+++ App.tsx
@@ -8,9 +8,13 @@ import {
 export default function App(){
   return(
     <View style={styles.Container}>
-      <Text style={styles.TextContainer}>Lista De Compra</Text>
-      <View>
-        
+      <View style={styles.ContainerTitle}>
+        <Text style={styles.TextContainer}>Lista De Compra</Text>
+      </View>
+      <View style={styles.boxList}>
+        <View style={styles.list}>
+          
+        </View>
       </View>
     </View>
   )
@@ -18,15 +22,38 @@ export default function App(){
 
 const styles = StyleSheet.create({
   Container: {
-    flex: 1,
+    flex: 1
+  },
+  ContainerTitle: {
     alignItems: "center",
     justifyContent: "flex-start",
-    marginTop: 35
+    marginTop: 35,
+    borderColor: "#F00000",
+    borderWidth: 1
     
   },
   TextContainer: {
     fontWeight: "bold",
     fontSize: 24,
     color: "#1B3A4B"
+  },
+  boxList: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1
+  },
+  list: {
+    width: "95%",
+    height: 238,
+    backgroundColor: "#Ffff",
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   }
 })
